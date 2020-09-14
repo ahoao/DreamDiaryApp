ff6301f1396878d3cade0cc65a850cd441868eb0
Ao.Nagasawa <aho_ao1004@yahoo.co.jp>
2020-09-14 15:39:04 +0900

    Initial Commit

---
+++ DreamDiaryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DreamDiaryApp
+//
+//  Created by Ao Nagasawa on 2020/09/14.
+//  Copyright © 2020 Ao.Nagasawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
